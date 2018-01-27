ff908d439ae3986ab50a793394b881fbc3f834c8
KyleBarts <Kpibartido@gmail.com>
2018-01-28 04:51:47 +0800

    Upload Activity

---
+++ bluehacks-mobile/app/src/main/java/com/titikcoe/www/bluehacks/Activities/UploadAudio.java
@@ -0,0 +1,50 @@
+package com.titikcoe.www.bluehacks.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.titikcoe.www.bluehacks.R;
+
+public class UploadAudio extends AppCompatActivity {
+    private EditText edtTitle;
+    private EditText edtDesc;
+    private EditText edtTags;
+    private Spinner mSpnGenre;
+    private Button btnProceed;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_audio);
+
+        edtTitle = (EditText) findViewById(R.id.edt_title);
+        edtDesc = (EditText) findViewById(R.id.edt_desc);
+        mSpnGenre = (Spinner) findViewById(R.id.spn_field);
+        edtTags = (EditText) findViewById(R.id.edt_desc);
+
+        btnProceed = (Button) findViewById(R.id.btn_proceed);
+
+        btnProceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent uploadIntent = new Intent(UploadAudio.this, RecordActivity.class);
+                uploadIntent.putExtra("title",edtTitle.getText().toString());
+                uploadIntent.putExtra("desc",edtDesc.getText().toString());
+                uploadIntent.putExtra("genre",mSpnGenre.getSelectedItem().toString());
+                uploadIntent.putExtra("tags" ,edtTags.getText().toString());
+                startActivity(uploadIntent);
+                finish();
+
+            }
+        });
+
+
+
+
+    }
+}
